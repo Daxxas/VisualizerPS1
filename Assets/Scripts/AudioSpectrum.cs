5aa1020091cee251269e34af62c41c641fe493fd
Jacques-Portable <daxxas158@gmail.com>
2022-11-28 22:44:29 +0100

    init commit

---
+++ Assets/Scripts/AudioSpectrum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lasp;
+using UnityEngine;
+
+public class AudioSpectrum : MonoBehaviour
+{
+    private float[] m_audioSpectrum;
+
+     public static float spectrumValue;
+     public float SetSpectrumValue { get => spectrumValue; set => spectrumValue = value; }
+
+    public static float SpectrumValue {get => spectrumValue; }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_audioSpectrum = new float[128];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+        
+        // if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
+        // {
+        //     spectrumValue = m_audioSpectrum[0] * 100;
+        // }
+
+        Debug.Log(spectrumValue);
+    }
+}
---
+++ Assets/Scripts/MMFPlayerSyncer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+[RequireComponent(typeof(MMF_Player))]
+public class MMFPlayerSyncer : AudioSyncer
+{
+    private MMF_Player player;
+
+    private void Start()
+    {
+        player = GetComponent<MMF_Player>();
+    }
+
+    public override void OnBeat()
+    {
+        base.OnBeat();
+        player.PlayFeedbacks();
+    }
+}
