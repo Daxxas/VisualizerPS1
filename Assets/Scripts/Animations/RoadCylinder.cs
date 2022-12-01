f406ba6b0f4609fd215a03eee18d3a517d080d5e
CleryJa <daxxas158@gmail.com>
2022-12-01 18:36:08 +0100

    added assets + road turning at good speed ~

--- Assets/Scripts/Animations/RoadCylinder.cs
+++ Assets/Scripts/Animations/RoadCylinder.cs
@@ -9,11 +9,23 @@ public class RoadCylinder : MonoBehaviour
 
     [SerializeField] private float speedCoefficient = 1.1f;
     [SerializeField] private float speedDeltaCoefficient = 1.1f;
+    [SerializeField] private float offset = 60f;
+    [SerializeField] private bool clamp = true;
+    [SerializeField] private float minValue = 0f;
+    [SerializeField] private float maxValue = 1f;
+    
     
     private void Update()
     {
-        float turnMultiplier = (((Mathf.Pow(audioLevelTracker.currentGain, speedDeltaCoefficient))) / 100f) * speedCoefficient;
-        Debug.Log("Turn Multiplier : " + turnMultiplier);
+        float turnMultiplier =  offset - ((audioLevelTracker.currentGain / speedCoefficient) * speedDeltaCoefficient);
+
+        if (clamp)
+        {
+            turnMultiplier = Mathf.Clamp(turnMultiplier, minValue, maxValue);
+        }
+        
+        turnMultiplier = -Mathf.Abs(turnMultiplier);
+        Debug.Log("Turn Multiplier : " + turnMultiplier + " | audioLevelTracker.currentGain : " + audioLevelTracker.currentGain);
         animator.SetFloat("turnMultiplier", turnMultiplier);
     }
 }
