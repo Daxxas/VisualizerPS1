1f2d38cf15b5d31accf0c80465fc3374b8e11b60
CleryJa <daxxas158@gmail.com>
2023-09-30 12:01:25 +0200

    push for sync

--- Assets/Scripts/WasapiBinder.cs
+++ Assets/Scripts/WasapiBinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Assets.WasapiAudio.Scripts.Core;
 using Assets.WasapiAudio.Scripts.Unity;
+using UnityEditor;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -8,7 +9,9 @@ namespace DefaultNamespace
     public class WasapiBinder : AudioVisualizationEffect
     {
         [SerializeField] private AudioSpectrum audioSpectrum;
-
+        [SerializeField] private bool autoGain = true;
+        
+        
         private void Update()
         {
             float[] spectrumData = GetSpectrumData();
@@ -21,6 +24,19 @@ namespace DefaultNamespace
             spectrumMeanValue /= spectrumData.Length;
             
             audioSpectrum.SpectrumValue = spectrumMeanValue;
+            
+             
+            
+            if (autoGain)
+            {
+                // // Slowly return to the noise floor.
+                // const float kDecaySpeed = 0.6f;
+                // _head = Mathf.Max(_head - kDecaySpeed * dt, kSilence);
+                //
+                // // Pull up by input with a small headroom.
+                // var room = _dynamicRange * 0.05f;
+                // _head = Mathf.Clamp(input - room, _head, 0);
+            }
             
         }
     }
